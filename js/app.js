483e54fff3471aed60ec53362b4a19f41a1e712e
Vinicius Terriani <viniterriani.vt@gmail.com>
2018-09-27 12:30:52 -0300

    landpage metal art version-1

---
+++ js/app.js
@@ -0,0 +1,152 @@
+
+//variables
+var parallax = $(".parallax-1");
+var splash_text = $(".splash-text");
+var body = $("body");
+var splash_screen = $(".splash-screen");
+var main = $("main");
+var header = $("header");
+var footer = $("footer");
+var doc = $(document);
+var win = $(window);
+var btn_up = $(".btn-up");
+var about_us = $(".about-us");
+var service = $(".services");
+var contact = $(".contact");
+var maps = $(".maps");
+var map = $(".map");
+var title_map = $(".title-map");
+var footer = $("footer");
+var card1 = $(".card-1");
+var card2 = $(".card-2");
+var card3 = $(".card-3");
+var title_service = $(".title-service");
+
+//initialize in page start
+$("document").ready(function(){
+    $(".button-collapse").sideNav();
+    $('.materialboxed').materialbox();
+    $(".scrollspy").scrollSpy();
+
+    splash_screen.css("background-color", "black");
+
+    about_us.css("opacity", 0);
+    about_us.css("transform", "scale(0.9)");
+
+    card1.css("opacity", 0)
+    card1.css("transform", "scale(0.9)");
+
+    card2.css("opacity", 0)
+    card2.css("transform", "scale(0.9)");
+
+    card3.css("opacity", 0)
+    card3.css("transform", "scale(0.9)");
+
+    contact.css("opacity", 0)
+    contact.css("transform", "scale(0.9)");
+
+    maps.css("transform", "scale(0.9)");
+
+    map.css("opacity", 0);
+
+    title_map.css("opacity", 0);
+
+    footer.css("opacity", 0);
+    
+    var options = {
+        strings: ["Portões automáticos", "Portas", "Janelas", "Box de vidro", "Espelhos", "Grades de ferro", "Portões de Ferro"],
+        typeSpeed: 80,
+        startDelay: 100,
+        backSpeed: 50,
+        loop: true
+
+      }
+      
+      var typed = new Typed(".typed", options);
+
+
+parallax.css("opacity", 1);
+splash_text.css("opacity", 1);
+main.css("display", "none");
+header.css("display", "none");
+footer.css("display", "none");
+
+$(".l1, .l3, .l5, .l7").css("top", 0);
+$(".l2, .l4, .l6, .l8").css("bottom", 0);
+
+setTimeout(function(){
+    $(".l1, .l3, .l5, .l7").css("top", "0");
+    $(".l2, .l4, .l6, .l8").css("bottom", "0");
+    splash_text.css("opacity", 0);
+},1800);
+
+
+setTimeout(function(){
+    body.css("overflow-y", "auto");
+    body.css("overflow-x", "hidden");
+    splash_screen.css("display", "none");
+    main.css("display", "block");
+    header.css("display", "block");
+    footer.css("display", "block");
+}, 3500);
+
+});
+
+
+doc.scroll(function(){
+    if (doc.scrollTop() > 150) {
+        btn_up.css("opacity", 1);
+        btn_up.css("bottom", "10px");
+    }else{
+        btn_up.css("bottom", "-100%");
+    }
+
+    if (window.scrollY > $("#about-us").offset().top - window.innerHeight / 2) {
+        about_us.css("opacity", 1);
+        about_us.css("transition", ".5s ease-out");
+        about_us.css("transform", "scale(1)");
+    }
+
+    if (window.scrollY > title_service.offset().top - window.innerHeight / 2) {
+        title_service.css("left", 0);
+    }
+
+    if (window.scrollY > card1.offset().top - window.innerHeight / 2) {
+        card1.css("opacity", 1);
+        card1.css("transition", ".2s ease-out");
+        card1.css("transform", "scale(1)");
+    }
+
+    if (window.scrollY > card2.offset().top - window.innerHeight / 2) {
+        card2.css("opacity", 1);
+        card2.css("transition", ".2s ease-out");
+        card2.css("transform", "scale(1)");
+    }
+
+    if (window.scrollY > card3.offset().top - window.innerHeight / 2) {
+        card3.css("opacity", 1);
+        card3.css("transition", ".2s ease-out");
+        card3.css("transform", "scale(1)");
+    }
+
+    if(window.scrollY > contact.offset().top - window.innerHeight / 2){
+        contact.css("opacity", 1);
+        contact.css("transition", ".5s ease-out");
+        contact.css("transform", "scale(1)");
+    }
+
+    if (window.scrollY > title_map.offset().top - window.innerHeight / 2) {
+        map.css("opacity", 1);
+        map.css("left", "0%");
+        map.css("transition", "1.5s ease-out")
+
+        title_map.css("opacity", 1);
+        title_map.css("right", "0%");
+        title_map.css("transition", "1s ease-out")
+    }
+
+    if (window.scrollY > title_map.offset().top) {
+        footer.css("opacity", 1);
+    }
+})
+
